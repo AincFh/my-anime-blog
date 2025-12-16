592ac9f94d562f72377fc75ac6442c12e1b00377
ainc fh <ljc2461167933@icloud.com>
2025-12-16 12:13:43 +0800

    fix: remove cloudflare preset, use default SSR

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,8 @@
 import type { Config } from "@react-router/dev/config";
-import { cloudflarePreset } from "@react-router/cloudflare";
 
 export default {
   ssr: true,
   future: {
     unstable_viteEnvironmentApi: true,
   },
-  presets: [cloudflarePreset()],
 } satisfies Config;
