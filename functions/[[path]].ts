d098c360742ff52eb976445102389b3b1c1f1f2d
ainc fh <ljc2461167933@icloud.com>
2025-12-17 10:31:25 +0800

    fix: replace import.meta.env.MODE with production string

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -3,7 +3,7 @@ import { createRequestHandler } from "react-router";
 // @ts-expect-error - React Router provides the server build at build time
 import * as serverBuild from "../build/server";
 
-const requestHandler = createRequestHandler(serverBuild, import.meta.env.MODE);
+const requestHandler = createRequestHandler(serverBuild, "production");
 
 export const onRequest: PagesFunction<Env> = async (context) => {
     return requestHandler(context.request, {
