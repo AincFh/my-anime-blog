849383bcf1553698d5bb7e63338b72bb7619d1a0
ainc fh <ljc2461167933@icloud.com>
2025-12-17 10:10:49 +0800

    fix: remove cloudflare vite plugin (not compatible with Pages)

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
@@ -6,7 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
